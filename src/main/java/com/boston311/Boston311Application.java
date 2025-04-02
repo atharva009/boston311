2ba85a19ddf85fff791ed0a76f8cc643d4770962
atharva009 <atharvahankare@gmail.com>
2025-04-02 18:52:00 -0400

    initial commit

---
+++ src/main/java/com/boston311/Boston311Application.java
@@ -0,0 +1,13 @@
+package com.boston311;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boston311Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boston311Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/boston311/Boston311ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boston311;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boston311ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
